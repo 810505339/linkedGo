e6ddab1713e1e13312f03da19cc58bcd8f3c831b
aliyun6590824919 <810505339@qq.com>
2024-05-26 20:19:50 +0800

    add

--- src/pages/mainScreen/user/orders/pay.tsx
+++ src/pages/mainScreen/user/orders/pay.tsx
@@ -17,11 +17,14 @@ import { cancelOrder } from "@api/order"
 import RNFS from 'react-native-fs';
 import Toast from "react-native-toast-message"
 import { CameraRoll } from "@react-native-camera-roll/camera-roll"
+import { useTranslation } from "react-i18next"
 
 const Pay = () => {
   const route = useRoute<RouteProp<RootStackParamList, 'Pay'>>()
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
   const qrCodeRef = useRef(null)
+
+  const { t } = useTranslation()
 
   const { orderId, orderStatus, codeUrl, codeExpireSecond } = route.params
   const { data, run, cancel } = useRequest(() => queryPayResult({ orderId }), {
@@ -95,7 +98,7 @@ const Pay = () => {
   return <BaseLayout source={bg}>
     <ScrollView className=" rounded-t-3xl bg-[#101010] flex-1">
       <View className=" py-5 px-14">
-        <Text className="text-center font-bold text-lg">待支付</Text>
+        <Text className="text-center font-bold text-lg">{t('orders.item1')}</Text>
         <Text className="text-[#FDAD25] font-bold text-2xl text-center my-2">S$1000.00</Text>
         <View className="flex-row items-center justify-center mt-5 mb-2.5 rounded overflow-hidden">
           <QRCode
@@ -106,15 +109,15 @@ const Pay = () => {
         </View>
         <View className="flex-row items-center justify-center mt-5 mb-2.5">
           <Button className="bg-[#FDAD25]  w-32 font-bold" textColor="#0C0C0CFF" labelStyle={{ fontWeight: 'bold' }} onPress={handleClick} >
-            下载二维码
+            {t('orders.item1')}
           </Button>
         </View>
-        <Text numberOfLines={3} className="pt-2.5 pb-5  text-white mb-2.5">您可以下载二维码并使用支持PayNow的App载入二维码进行支付，也可以使用另一个手机直接扫码支付。</Text>
+        <Text numberOfLines={3} className="pt-2.5 pb-5  text-white mb-2.5">{t('pay.tips')}</Text>
         <Divider />
         <View className="mt-10">
-          <Text className=" text-center  opacity-75  mb-2.5 text-xs">如果支付成功后页面没有自动跳转，请点击以下按钮</Text>
-          <Button textColor="#FDAD25" labelStyle={{ fontWeight: 'bold' }} onPress={run}>我已经完成付款</Button>
-          <Button textColor="#ffffff" labelStyle={{ fontWeight: 'bold' }} onPress={cancelPay}>取消支付</Button>
+          <Text className=" text-center  opacity-75  mb-2.5 text-xs">{t('pay.tips1')}</Text>
+          <Button textColor="#FDAD25" labelStyle={{ fontWeight: 'bold' }} onPress={run}>{t('pay.btn1')}</Button>
+          <Button textColor="#ffffff" labelStyle={{ fontWeight: 'bold' }} onPress={cancelPay}>{t('pay.btn2')}</Button>
         </View>
       </View>
     </ScrollView>
