f00c93112f4c20ecaa9833202e90b6dedc53c23c
aliyun6590824919 <810505339@qq.com>
2024-06-29 20:00:09 +0800

    add

--- src/components/baselayout/index.tsx
+++ src/components/baselayout/index.tsx
@@ -46,17 +46,16 @@ const BaseLayout: FC<PropsWithChildren<IProps>> = ({ source = defaultBg, classNa
 
   const classNames = `flex-1 bg-[#101010FF] ${className}`;
   return (
-    <SafeAreaView className='border' style={{ flex: 1 }}>
-      <View className={classNames} onStartShouldSetResponder={handleUnhandledTouches}>
-        {loading && <Loading />}
-        {source && <ImageBackground source={source} resizeMode="cover" className="absolute left-0 right-0 bottom-0 -z-10 top-0" />}
-        {showAppBar && <View style={{ paddingTop: statusBarHeight }} />}
-        {/* {showNoMore ? <RendernoMoreData /> : children} */}
-        {showNoMore ? <RendernoMoreData /> : children}
-        {/* {showNoMore ? <RendernoMoreData /> : children} */}
 
-      </View >
-    </SafeAreaView>
+    <View className={classNames} onStartShouldSetResponder={handleUnhandledTouches}>
+      {loading && <Loading />}
+      {source && <ImageBackground source={source} resizeMode="cover" className="absolute left-0 right-0 bottom-0 -z-10 top-0" />}
+      {showAppBar && <View style={{ paddingTop: statusBarHeight }} />}
+      {/* {showNoMore ? <RendernoMoreData /> : children} */}
+      {showNoMore ? <RendernoMoreData /> : <SafeAreaView style={{ flex: 1 }}>{children}</SafeAreaView>}
+      {/* {showNoMore ? <RendernoMoreData /> : children} */}
+    </View >
+
   );
 };
 
